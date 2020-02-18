609a2d6f4c9c07d76402ee3ab299b30fe762861f
DevDBreno <dbrenoti@gmail.com>
2020-02-18 16:03:49 -0300

    init: structure

---
+++ src/utils/index.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response, NextFunction } from 'express'
+
+type Wrapper = (router: Router) => void
+
+type Handler = (req: Request, res: Response, next: NextFunction) => Promise<void> | void
+
+type Route = {
+  path: string
+  method: string
+  handler: Handler | Handler[]
+}
+
+export const applyMiddleware = (middlewareWrappers: Wrapper[], router: Router) =>
+  middlewareWrappers.forEach(wrapper => wrapper(router))
+
+export const applyRoutes = (routes: Route[], router: Router) =>
+  routes.map(route => {
+    const { method, path, handler } = route
+    router[method](path, handler)
+  })
